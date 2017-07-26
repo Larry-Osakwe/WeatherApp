3d8f2fef6412f0dbd60679073dc763599bb68b1c
Larry <larryosak@gmail.com>
2017-07-26 07:39:45 -0700

    Populate weather array with dummy data

--- app/src/main/java/com/larry/osakwe/weatherapp/MainActivity.java
+++ app/src/main/java/com/larry/osakwe/weatherapp/MainActivity.java
@@ -1,13 +1,24 @@
 package com.larry.osakwe.weatherapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTextWeatherView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextWeatherView = (TextView)findViewById(R.id.tv_weather_data);
+
+        String[] fakeWeatherData = {"Today, May 17 - Clear - 17°C / 15°C", "Tomorrow - Cloudy - 19°C / 15°C", "what", "you", "want?"};
+        
+        for (String fakeWeather: fakeWeatherData) {
+            mTextWeatherView.append(fakeWeather + "\n\n\n");
+        }
     }
 }
